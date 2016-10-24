d9de20374c9b34b25465f55ca7f8a9e8d301cb17
Chris Blythe <chris@blythe.plus.com>
2016-10-25 00:02:24 +0100

    Change name error

--- WindowsFormsCPPTest/WindowsFormsCPPTest/MyForm.cpp
+++ WindowsFormsCPPTest/WindowsFormsCPPTest/MyForm.cpp
@@ -2,6 +2,10 @@
 
 // Initial setup guide: https://www.youtube.com/watch?v=AP8Tz9RfbxE
 // Number generator tutorial: http://www.bogotobogo.com/cplusplus/application_visual_studio_2013.php
+// NOTE on above tutorial - DEPLOYMENT SECTION
+	// Setup and Deployment (InstallShield) is not supported in VS2015 Community edition
+	// Suggestion to try from http://stackoverflow.com/questions/31888465/visual-studio-2015-community-isle-setup-and-deployment-doesnt-appear:
+	// Microsoft Visual Studio 2015 Installer Projects https://visualstudiogallery.msdn.microsoft.com/f1cc3f3e-c300-40a7-8797-c509fb8933b9
 
 using namespace System;
 using namespace System::Windows::Forms;
